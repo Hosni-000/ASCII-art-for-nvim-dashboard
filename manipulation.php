563c7f2316784fe811e08b97874e7ddc82db6d69
Hosni <hosnyali070@gmail.com>
2026-06-01 08:24:24 +0300

    final script

--- manipulation.php
+++ manipulation.php
@@ -1,10 +1,15 @@
 #! /usr/bin/php
 <?php
+// defining path of ascii art
+// please put the art in that file name "draft.php"
 $draft_file = __DIR__ . "/draft.txt";
 
 if (!file_exists($draft_file)) {
 	die("Please make a file named draft.txt");
 }
+
+// important to read eachline and ignoring the newline sign 
+// that will ruin the art
 $file = file($draft_file, FILE_IGNORE_NEW_LINES);
 echo "dashboard.section.header.val = {" . PHP_EOL;
 
